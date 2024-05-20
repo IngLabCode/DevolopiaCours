b9cc9038b1fb1e4b7ef9b1e6c894eff0ef103198
Inqilab Aliyev <eliyevinqlab.02@gmail.com>
2024-05-20 16:02:07 +0400

    Modificators

---
+++ JavaLessons/src/main/java/Lesson8/ModifiersDemo.java
@@ -0,0 +1,51 @@
+package Lesson8;
+
+public class ModifiersDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Person {
+    static String faculty;
+    int age;
+    private int id;
+    protected String name;
+    public static final String surname = "aliyev";
+
+    private void modif1() {
+
+    }
+
+    public void modif2() {
+
+    }
+
+    protected void modif3() {
+
+    }
+
+    final void modif4() {
+
+    }
+
+    public Person() {
+
+    }
+
+    protected Person(final int name) {
+
+    }
+
+    private Person(String anme) {
+
+    }
+
+    Person(int id, String name) {
+
+    }
+
+    public static final void modif5() {
+
+    }
+}
