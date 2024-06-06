64d0e2ef7ff20e360db41a46aeeaaee4637b42c8
Inqilab Aliyev <eliyevinqlab.02@gmail.com>
2024-06-06 16:52:41 +0400

    Lesson13

---
+++ JavaLessons/src/main/java/Lesson13/EvIsi1/Animal.java
@@ -0,0 +1,21 @@
+package Lesson13.EvIsi1;
+
+public abstract class Animal {
+    private String name;
+    private int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public abstract String makeNoise();
+}
---
+++ JavaLessons/src/main/java/Lesson13/EvIsi1/Lion.java
@@ -0,0 +1,12 @@
+package Lesson13.EvIsi1;
+
+public class Lion extends Animal {
+    public Lion(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String makeNoise() {
+        return "sir ses cixarir";
+    }
+}
