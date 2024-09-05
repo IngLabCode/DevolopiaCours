883b469e727e7ee52617a055bafccb61070fc94a
Inqilab Aliyev <eliyevinqlab.02@gmail.com>
2024-09-05 18:07:47 +0400

    config application properties

--- librarian/src/main/java/com/example/librarian/entity/BookEntity.java
+++ librarian/src/main/java/com/example/librarian/entity/BookEntity.java
@@ -1,13 +1,11 @@
 package com.example.librarian.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "books")
 public class BookEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
