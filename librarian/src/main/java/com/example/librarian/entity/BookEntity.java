dafde6005fe62e1027152a1d16f07adf4c1b6020
Inqilab Aliyev <eliyevinqlab.02@gmail.com>
2024-09-05 18:00:25 +0400

    book entity is created

---
+++ librarian/src/main/java/com/example/librarian/entity/BookEntity.java
@@ -0,0 +1,19 @@
+package com.example.librarian.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer pageCount;
+    private String author;
+    private String genre;
+}
