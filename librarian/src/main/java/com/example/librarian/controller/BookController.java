98e106318a6ae6e3387018097b9706512cc4beee
Inqilab Aliyev <eliyevinqlab.02@gmail.com>
2024-09-05 18:34:23 +0400

    add API  is created

--- librarian/src/main/java/com/example/librarian/controller/BookController.java
+++ librarian/src/main/java/com/example/librarian/controller/BookController.java
@@ -1,7 +1,21 @@
 package com.example.librarian.controller;
 
+import com.example.librarian.dto.request.BookAddRequestDTO;
+import com.example.librarian.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
 public class BookController {
+    private final BookService bookService;
+
+    @PostMapping("/add")
+    public void add(@RequestBody BookAddRequestDTO bookAddRequestDTO){
+        bookService.addBook(bookAddRequestDTO);
+    }
 }
