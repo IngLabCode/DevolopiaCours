2ea9385d3b666f883b2bb0861cf627b0e0737070
Inqilab Aliyev <eliyevinqlab.02@gmail.com>
2024-09-05 18:25:41 +0400

    ModelMapper is configurated

--- librarian/src/main/java/com/example/librarian/service/BookService.java
+++ librarian/src/main/java/com/example/librarian/service/BookService.java
@@ -1,4 +1,7 @@
 package com.example.librarian.service;
 
+import com.example.librarian.dto.request.BookAddRequestDTO;
+
 public interface BookService {
+
 }
--- librarian/src/main/java/com/example/librarian/service/impl/BookServiceImpl.java
+++ librarian/src/main/java/com/example/librarian/service/impl/BookServiceImpl.java
@@ -1,6 +1,12 @@
 package com.example.librarian.service.impl;
 
+import com.example.librarian.dto.request.BookAddRequestDTO;
+import com.example.librarian.entity.BookEntity;
 import com.example.librarian.service.BookService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService {
+
 }
