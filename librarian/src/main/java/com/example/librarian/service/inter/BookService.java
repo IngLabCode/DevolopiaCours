6db3db7c25ea285dbb9973b3570acc5b60a25b2e
Inqilab Aliyev <eliyevinqlab.02@gmail.com>
2024-09-11 16:42:46 +0400

    findAll method is created in Bookservice

--- librarian/src/main/java/com/example/librarian/service/inter/BookService.java
+++ librarian/src/main/java/com/example/librarian/service/inter/BookService.java
@@ -1,9 +1,11 @@
 package com.example.librarian.service.inter;
 
 import com.example.librarian.dto.request.BookAddRequestDTO;
+import com.example.librarian.dto.respond.BookFindAllRespondDTO;
 
 public interface BookService {
     void addBook(BookAddRequestDTO bookAddRequestDTO);
 
+    BookFindAllRespondDTO findAll();
 
 }
