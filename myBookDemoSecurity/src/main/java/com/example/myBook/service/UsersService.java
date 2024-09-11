527b74e89b5e55bc1fb8defbc8e566beec7629da
inglab <117545612+IngLabCoder@users.noreply.github.com>
2024-09-11 21:18:35 +0400

    Delete myBookDemoSecurity/src/main/java/com/example/myBook directory

--- myBookDemoSecurity/src/main/java/com/example/myBook/service/UsersService.java
+++
@@ -1,7 +0,0 @@
-package com.example.myBook.service;
-
-import com.example.myBook.dto.request.UserAddRequestDTO;
-
-public interface UsersService {
-    void add(UserAddRequestDTO userAddRequestDTO);
-}
